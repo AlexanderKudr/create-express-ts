a744e87234d4478c850cc459b9faf0fce6859dcd
Alex <trevan98@gmail.com>
2023-10-23 20:28:53 +0300

    update starter to fully ts environment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import * as dotenv from "dotenv";
+import { testData } from "./module/test";
 
 dotenv.config();
 const app = express();
@@ -8,10 +9,11 @@ const port = 8080;
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+  console.log("also here is the data " + testData);
+});
